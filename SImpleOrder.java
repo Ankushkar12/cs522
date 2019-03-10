194c4d36295bd5630a08bc2baceb555ab99546a9
Ankush Kar <46693881+Ankushkar12@users.noreply.github.com>
2019-03-09 18:09:52 -0800

    Practice
    After compiling these files, you invoke the Javadoc tool using this command:
    
    javadoc -private -doclet codegen.samples.SimpleDoclet SimpleOrder.java
    
    Loading source file SimpleOrder.java...
    Constructing Javadoc information...
    Methods
    -------
    Method: name = getSymbol
    Method: name = getQuantity
    Fields
    ------
    Field: name = Symbol, comment = A valid stock symbol., type = 
    java.lang.String;
          Field Tag Name= @see
          Field Tag Value = A big book of valid symbols for more information.
    Field: name = Quantity, comment = The total order volume., type = int;
          Field Tag Name= @mytag
          Field Tag Value = My custom tag.
    Field: name = OrderType, comment = , type = java.lang.String;
    Field: name = Price, comment = , type = float;
    Field: name = Duration, comment = , type = java.lang.String;
    Field: name = AccountType, comment = , type = int;
    Field: name = TransactionType, comment = , type = int;

---
+++ SImpleOrder.java
@@ -0,0 +1,26 @@
+public class SimpleOrder  {
+  public SimpleOrder() { }
+  public String getSymbol() {
+    return Symbol;
+  }
+  public int getQuantity() {{escriptive
+    return Quantity;
+  }
+  /**
+   * A valid stock symbol.
+   *
+   * @see A big book of valid symbols for more information.
+   */
+  private String Symbol;
+  /**
+   * The total order volume.
+   *
+   * @mytag My custom tag.
+   */
+  private int Quantity;
+  private String OrderType;
+  private float Price;
+  private String Duration;
+  private int AccountType;
+  private int TransactionType;
+}
